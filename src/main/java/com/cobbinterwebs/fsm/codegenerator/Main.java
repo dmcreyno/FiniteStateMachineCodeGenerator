8e5136075c50d84d2296ff7316c4ad5a2c2facbe
David McReynolds <david.mcreynolds@gmail.com>
2022-09-29 00:20:35 -0400

    zxczxczxczxc

--- src/main/java/com/cobbinterwebs/fsm/codegenerator/Main.java
+++ src/main/java/com/cobbinterwebs/fsm/codegenerator/Main.java
@@ -1,7 +1,5 @@
 package com.cobbinterwebs.fsm.codegenerator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
@@ -15,19 +13,7 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 
- * Reads a definition of a Finite State Machine from an XML file. Generates code
- * for the events to move state.
- * 
- * The README file has details and examples.
- * 
- * @author Cobb Interwebs, LLC
- *
- */
 public class Main {
-	
-	private final Logger log = LoggerFactory.getLogger("");
 
     private static final int CL_INPUT_FILE_IDX = 0;
     private static final int CL_OUTPUT_FOLDER_IDX = 1;
@@ -46,13 +32,12 @@ public class Main {
     static PrintWriter printWriter = null;
 
     public Main(String inputFileName, String outputFolderName) {
-    	log.info("Creating application with arguments: \"{}\" and \"{}\".", inputFileName, outputFolderName);
         this.inputFileName = inputFileName;
         this.outputFolderName = outputFolderName;
     }
 
     public static void main(String[] fileNames) {
-        // check arguments. Make sure there are two.
+        // check arguements. Make sure there are two.
         if(2 != fileNames.length) {
             showUsage();
             System.exit(-1);
@@ -86,16 +71,16 @@ public class Main {
 		File filePath = new File(path);
 		
 		if(filePath.exists()) {
-			log.info("Directory already exists: {}", filePath.getCanonicalPath());
+			System.out.println("Directory already exists: " + filePath.getCanonicalPath());
 			return;
 		}
 		
 		boolean status = filePath.mkdirs();
 		
 		if(status) {
-			log.info("Directory created successfully: {}", filePath.getCanonicalPath());
+			System.out.println("Directory created successfully");
 		} else {
-			log.info("Sorry couldnt create specified directory: {}", filePath.getCanonicalPath());
+			System.out.println("Sorry couldnt create specified directory");
 			throw new IOException();
 		}
 	}
@@ -103,20 +88,16 @@ public class Main {
 	private void initPackageClassVars() {
         implName =    doc.getElementsByTagName("fsm").item(0).getAttributes().getNamedItem("implClass").getNodeValue();
         packageName = doc.getElementsByTagName("fsm").item(0).getAttributes().getNamedItem("package").getNodeValue();
-        log.info("implName: {}", implName);
-        log.info("packageName: {}", packageName);
     }
     
     private void openInputFile() throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         doc = builder.parse(new File(inputFileName));
         doc.getDocumentElement().normalize();
-        log.info("opened xml document: {}", inputFileName);
     }
 
     private void openOutputFile() throws FileNotFoundException {
         printWriter = new PrintWriter(path + "/" + implName + ".java");
-        log.info("opened java class file: {}", path + "/" + implName + ".java");
     }
 
     private static void showUsage() {
@@ -127,17 +108,11 @@ public class Main {
     }
 
     void generateCode() throws ParserConfigurationException, IOException, SAXException {
-    	log.info("generateCode");
         Node FSM = doc.getElementsByTagName("fsm").item(0);
         Node implClassNode = FSM.getAttributes().getNamedItem("implClass");
         implName = implClassNode.getNodeValue();
         
         printWriter.println("package " + packageName + ";\n\n");
-    	printWriter.println("/**");
-    	printWriter.println(" * If generator is executed this file will be overwritten.");
-    	printWriter.println(" *");
-    	printWriter.println(" * @author Cobb Interwebs, LLC.");
-    	printWriter.println(" */");
         printWriter.println("public class " + implName + " {");
         printWriter.println("   private STATE _state;");
         processStateNames();
@@ -145,7 +120,6 @@ public class Main {
     }
 
     private void processStateNames() {
-    	log.info("processStateNames");
         NodeList states = doc.getElementsByTagName("state");
         printWriter.print("   private enum STATE{");
         int stateLen = states.getLength();
@@ -153,7 +127,6 @@ public class Main {
             Node aStateNode = states.item(i);
             NamedNodeMap attributeMap = aStateNode.getAttributes();
             stateName = attributeMap.getNamedItem("name").getNodeValue();
-        	log.info("generating enum for {}", stateName );
             printWriter.print(stateName);
             if(i == stateLen - 1) {
                 continue;
@@ -164,14 +137,13 @@ public class Main {
 
         for (int i = 0; i < stateLen; i++) {
            Node aStateNode = states.item(i);
+            NamedNodeMap attributeMap = aStateNode.getAttributes();
+            stateName = attributeMap.getNamedItem("name").getNodeValue();
             processEventsForAState(aStateNode);
         }
     }
 
     private void processEventsForAState(Node stateNode) {
-        NamedNodeMap attributeMap = stateNode.getAttributes();
-        stateName = attributeMap.getNamedItem("name").getNodeValue();
-    	log.info("processEventsForAState: {}", stateName);
         NodeList eventNodes = stateNode.getChildNodes();
         int eventLen = eventNodes.getLength();
         for (int i = 0; i < eventLen; i++) {
@@ -180,24 +152,14 @@ public class Main {
                 NamedNodeMap attrs = eventNode.getAttributes();
                 String handlerNameFunction = attrs.getNamedItem("handlerFunction").getNodeValue();
                 if(null != handleFunctionNames.get(handlerNameFunction)) {
-                    continue; // we've already processed this event once before
+                    continue;
                 }
-
                 handleFunctionNames.put(handlerNameFunction, handlerNameFunction);
-            	log.info("state, event, resultState, handlerFunction: {}, {}, {}, {}", stateName, attrs.getNamedItem("name").getNodeValue(), attrs.getNamedItem("resultState").getNodeValue(), attrs.getNamedItem("handlerFunction").getNodeValue());
-
-            	printWriter.println("   /**");
-            	printWriter.println("    *");
-            	printWriter.println("    *");
-            	printWriter.println("    *");
-            	printWriter.println("    */");
-            	printWriter.println("   public void " + attrs.getNamedItem("handlerFunction").getNodeValue() + "() throws IllegalStateException{");
+                //<event name="timeout" resultState="closed" handlerFunction="Timeout"/>
+                printWriter.println("   public void " + attrs.getNamedItem("handlerFunction").getNodeValue() + "() throws IllegalStateException{");
                 printWriter.println("      if( this._state != STATE." + this.stateName + "){");
                 printWriter.println("         throw new IllegalStateException();");
                 printWriter.println("      }\n");
-
-                printWriter.println("      // TODO insert business/system logic to transition state\n");
-
                 printWriter.println("      _state = STATE." + attrs.getNamedItem("resultState").getNodeValue() + ";");
                 printWriter.println("   }\n");
             }
